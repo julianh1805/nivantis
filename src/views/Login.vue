0e6ce84284ae3d4d3b2d6961d8c57a287b009e0a
LoganLS <logan_56@hotmail.fr>
2020-07-01 19:19:20 +0200

    Formulaires front connexion Nivantis et DMO

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="login">
     <h1>Connexion {{ $route.params.espace }}</h1>
-    <form>
+
+    <form v-if="$route.params.espace===dmo">
         <div>
             <label>Email</label>
             <input type="mail" name="email" placeholder="Adresse email" v-model="email" required/>
@@ -11,7 +12,24 @@
             <input type="password" name="password" v-model="password" required/>
         </div>
 
-        <div class="submit button" @click.prevent="login">Se connecter</div>
+        <div class="submit button" @click.prevent="loginDmo">Se connecter</div>
+
+        <!--<div class="errors" v-bind:if="hasErrors">
+            <p v:bind-for="error in errors">{{ error }}</p>
+        </div>-->
+    </form>
+
+    <form v-else>
+        <div>
+            <label>Email</label>
+            <input type="mail" name="email" placeholder="Adresse email" v-model="email" required/>
+        </div>
+        <div>
+            <label>Mot de passe</label>
+            <input type="password" name="password" v-model="password" required/>
+        </div>
+
+        <div class="submit button" @click.prevent="loginNivantis">Se connecter</div>
 
         <!--<div class="errors" v-bind:if="hasErrors">
             <p v:bind-for="error in errors">{{ error }}</p>
@@ -37,7 +55,19 @@ export default {
       }
   },
   methods: {
-      login () {
+      loginNivantis () {
+          console.log('login')
+          this.errors=[]
+          if(this.isFormValid()){
+              firebase.auth().signInWithEmailAndPassword(this.email,this.password).then(user => {
+                  this.$store.dispatch('setUser',user)
+                  this.$router.push('/dmo/Home')
+              }).catch(error => {
+                  this.errors.push(error.message)
+              })
+          }
+      },
+      loginDmo () {
           console.log('login')
           this.errors=[]
           if(this.isFormValid()){
