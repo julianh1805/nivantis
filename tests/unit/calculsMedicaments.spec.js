6729a272ae6dba46ced00e46eb97103f6145badc
LoganLS <logan_56@hotmail.fr>
2020-07-02 11:16:00 +0200

    test unit calculs medicaments

--- tests/unit/calculsMedicaments.spec.js
+++ tests/unit/calculsMedicaments.spec.js
@@ -1,12 +1,13 @@
+//import { jest } from '@vue/cli-plugin-unit-jest'
 import { expect } from 'chai'
-import { jest } from '@vue/cli-plugin-unit-jest'
 import { shallowMount } from '@vue/test-utils'
 import Calcul from '@/views/dmo/Calcul.vue'
 //import { iterator } from 'core-js/fn/symbol'
 
-it('should emit an event', ()=>{
+it('should emit an event',()=>{
     //given
     const wrapper = shallowMount(Calcul)
+    //const select_medicaments = wrapper.find('#select')
     const select_medicaments = wrapper.findAll('#select option')
     const spy = jest.fn()
 
