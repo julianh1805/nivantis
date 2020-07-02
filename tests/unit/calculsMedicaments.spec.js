4fd45f1d4cf442763a8705c957eba30bc39cf381
LoganLS <logan_56@hotmail.fr>
2020-07-02 10:59:59 +0200

    le test du test unitaire

---
+++ tests/unit/calculsMedicaments.spec.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import { jest } from '@vue/cli-plugin-unit-jest'
+import { shallowMount } from '@vue/test-utils'
+import Calcul from '@/views/dmo/Calcul.vue'
+//import { iterator } from 'core-js/fn/symbol'
+
+it('should emit an event', ()=>{
+    //given
+    const wrapper = shallowMount(Calcul)
+    const select_medicaments = wrapper.findAll('#select option')
+    const spy = jest.fn()
+
+    //when
+    select_medicaments.at(1).trigger('change')
+
+    //then
+    expect(spy).toHaveBeenCalledTimes(1);
+});
