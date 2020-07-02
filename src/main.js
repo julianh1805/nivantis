09105f4f658c18acd7189d9238802837da759d8d
Louis BESSE <louisdoubi@gmail.com>
2020-07-02 13:04:52 +0200

    fix main

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import {
 } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { rtdbPlugin } from 'vuefire'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
@@ -22,4 +23,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-
